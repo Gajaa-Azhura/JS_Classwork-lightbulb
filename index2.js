345d434d87849f642353f8e0389610844a38c3bd
Gajaa-Azhura <shinr433@gmail.com>
2025-03-13 12:04:13 +0530

    lightbub

---
+++ index2.js
@@ -0,0 +1,6 @@
+function change() {
+    document.getElementById("lightbulb").src = "./images/Screenshot 2025-03-12 150806.png";
+}
+function changeBack() {
+    document.getElementById("lightbulb").src = "./images/Screenshot 2025-03-12 150755.png";
+}
