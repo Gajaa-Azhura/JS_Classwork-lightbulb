8c5eed93ebf7c95576b50eaed492c8bef1c23d5e
Gajaa-Azhura <shinr433@gmail.com>
2025-03-18 11:09:19 +0530

    minor changes

--- index2.js
+++ index2.js
@@ -1,6 +1,6 @@
-function change() {
+function turnOn() {
     document.getElementById("lightbulb").src = "./images/Screenshot 2025-03-12 150806.png";
 }
-function changeBack() {
+function turnOff() {
     document.getElementById("lightbulb").src = "./images/Screenshot 2025-03-12 150755.png";
 }
